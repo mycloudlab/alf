45c8d613d4b34c5270c14d32664bfbf8db3a4444
Carlos Alberto <carlos.rodrigues@capes.gov.br>
2018-05-03 15:19:18 -0300

    adicionado suporte a mqtt via websocket

--- mqtt-server/docker-entrypoint.sh
+++ mqtt-server/docker-entrypoint.sh
@@ -3,6 +3,7 @@
 cat <<EOF > /mosquitto/config/mosquitto.conf
 allow_anonymous false 
 password_file /mosquitto/config/mosquitto_passwd
+protocol websockets
 EOF
 
 echo $AUTH > /mosquitto/config/mosquitto_passwd
